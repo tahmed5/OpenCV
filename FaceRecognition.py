bbf8c2cc2430cf356e505216fbde33da1eabd82f
tahmed5 <31918795+tahmed5@users.noreply.github.com>
2018-08-10 09:00:34 +0100

    Add files via upload

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -1,6 +1,14 @@
 import numpy as np
 import cv2
 import pickle
+import random
+
+want = str(input('DO YOU WANT?\n'))
+user = None
+
+if want.lower() == 'y':
+    user = str(input('Who Are You?\n'))
+    
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
 recognizer = cv2.face.LBPHFaceRecognizer_create()
@@ -11,13 +19,14 @@ with open("labels.pickle", 'rb') as f: #wb = writing bytes
     labels = {v:k for k,v in labels.items()}
 
 print(labels)
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
 
 while True:
     ret, frame = cap.read()
+    retval, wframe = cap.read()
     grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(grey, scaleFactor = 1.5, minNeighbors = 5)
-
+    
     for (x, y, w, h) in faces: #region of interest = x,y,w,h
         roi_grey = grey[y:y+h, x:x+w] #returns all the pixels from x,y plus the width and height selecting a specfic region
         roi_frame = frame[y:y+h, x:x+w]
@@ -32,7 +41,14 @@ while True:
             colour = (255,255,255)
             stroke = 2
             cv2.putText(frame,name,(x,y), font, 1, colour,stroke,cv2.LINE_AA)
-            
+            print(name)
+
+        key = cv2.waitKey(19)
+
+        if key == ord(' ') and user != None:
+            rint = random.randint(0, 9999999)
+            cv2.imwrite('./images/{}/{}.jpg'.format(user,rint), wframe)
+
         #img_item = "my-image.png"
         #cv2.imwrite(img_item, roi_frame)
 
